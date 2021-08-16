6b091fd9edcb2db43fc554366fe334c56a14a387
YenChenWang <s1310931001@gms.nutc.edu.tw>
2021-08-16 22:16:33 +0800

    取得特定的 unix timestamp

--- unix timestamp/js/all1.js
+++ unix timestamp/js/all1.js
@@ -1,4 +1,8 @@
-const dateTime = Date.now();
+// const dateTime = Date.now();
+// const timestamp = Math.floor(dateTime / 1000);
+
+// 取得特定的 unix timestamp 傳送一個 YYYY-MM-DD 或 YYYY-MM-DDT00:00:00Z 參數到 Date 的建構子。
+const dateTime = +new Date('2021-08-15T12:54:00Z');
 const timestamp = Math.floor(dateTime / 1000);
 
 console.log(dateTime);
